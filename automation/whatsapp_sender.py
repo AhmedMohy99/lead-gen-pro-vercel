e7ac58c7354fb54eb811b86534fabc122c5d19e1
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-07 23:57:16 +0200

    Delete automation directory

--- automation/whatsapp_sender.py
+++
@@ -1,8 +0,0 @@
-from urllib.parse import quote
-from config import WHATSAPP_MESSAGE
-
-
-
-def build_whatsapp_link(phone: str) -> str:
-    cleaned_phone = ''.join(ch for ch in phone if ch.isdigit() or ch == '+').lstrip('+')
-    return f'https://wa.me/{cleaned_phone}?text={quote(WHATSAPP_MESSAGE)}'
